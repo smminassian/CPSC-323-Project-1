3ac8bf1e784faa340dc099f9b46bfce90cc15173
smminassian <143156014+smminassian@users.noreply.github.com>
2025-09-23 08:18:31 -0700

    just added a bit of code, plan to add more later

---
+++ Lexical_Analyzer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int lexer(char* source_code[]){
+    // Implement lexical analysis here. We return lexeme and token later
+    return 0;
+}
+
+
+int main(int argc, char* argv[]){
+   //main logic will go here. 
+   return 0;
+}
