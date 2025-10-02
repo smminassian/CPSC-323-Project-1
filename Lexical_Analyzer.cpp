0ce209763cc1cc84c3bce2943aeb582d92dc3410
Gabriel Pacquing <gdpacquing@csu.fullerton.edu>
2025-10-02 00:38:02 -0700

    Completed FSM for Identifier, Integer, and Real

--- Lexical_Analyzer.cpp
+++ Lexical_Analyzer.cpp
@@ -1,12 +1,107 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cctype>
 using namespace std;
 
 
 int lexer(char* token){
     // Implement lexical analysis here. We return lexeme and token later
     return 0;
+}
+
+const int IdBeginning = 0;
+const int IdValid = 1;
+const int IdInvalid = 2;
+
+string IdentifierFSM (const string& input){
+	int state = IdBeginning;
+	
+	for (size_t  i = 0; i < input.size(); i++){
+		char ch = input[i];
+		
+		switch (state) {
+			case IdBeginning:
+			if (isalpha(ch) == true) {
+				state = IdValid;
+			} else {
+				state = IdInvalid;
+			}
+			break;
+				
+			case IdValid:
+			if (isalnum(ch) == true) {
+				state = IdValid;
+			} else {
+				state = IdInvalid;
+			}
+			break;
+				
+			case IdInvalid:
+				break;
+		}
+		if (state == IdInvalid) {break;}
+	}
+	
+	if (state == IdValid) {
+		return "identifier " + input;
+	} else { return "invalid"; }
+		
+	
+}
+
+const int NumBeginning = 0;
+const int NumInt = 1;
+const int NumReal = 2;
+const int NumInvalid = 3;
+
+string NumberFSM (const string& input){
+	int state = NumBeginning;
+	
+	for (size_t  i = 0; i < input.size(); i++){
+		char ch = input[i];
+		
+		switch (state) {
+			case NumBeginning:
+			if (isnum(ch) == true) {
+				state = NumInt;
+			} else if (ch == ".") {
+				state = NumReal;
+			} else {
+				state = NumInvalid;
+			}
+			break;
+				
+			case NumInt:
+			if (isnum(ch) == true) {
+				state = NumInt;
+			} else if (ch == ".") {
+				state = NumReal;
+			} else {
+				state = NumInvalid;
+			}
+			break;
+			
+			case NumReal:
+			if (isnum(ch) == true) {
+				state = NumReal;
+			} else {
+				state = NumInvalid;
+			}
+			
+			case NumInvalid:
+				break;
+		}
+		if (state == NumInvalid) {break;}
+	}
+	
+	if (state == NumInt) {
+		return "integer " + input;
+	} else if (state == NumReal) {
+		return "real " + input;
+	} else { return "invalid"; }
+		
+	
 }
 
 
