3238096ead0dbda6c151742238c818d1672502d2
smminassian <143156014+smminassian@users.noreply.github.com>
2025-10-04 17:19:57 -0700

    ok i finished up main and im working on my lexer function now. Copilot helped me understand it all and now i get how this thing works

--- Lexical_Analyzer.cpp
+++ Lexical_Analyzer.cpp
@@ -4,122 +4,177 @@
 #include <cctype>
 using namespace std;
 
+struct Token
+{
+	char token[20];
+	char lexeme[20];
+};
 
-int lexer(char* token){
-    // Implement lexical analysis here. We return lexeme and token later
-    return 0;
+Token lexer(ifstream &myFile)
+{
+	Token t;
+	char ch;
+	int ix = 0;
+
+	while (myFile.get(ch))
+	{
+		if (ch == ' ' || ch == '\n' || ch == '\t')
+		{
+			t.lexeme[ix] = '\0';
+			//call FSM functions here
+			
+			continue;
+			ix = 0;
+		}
+		t.lexeme[ix] = ch;
+		ix++;
+	}
 }
 
-const int IdBeginning = 0;
-const int IdValid = 1;
-const int IdInvalid = 2;
+	const int IdBeginning = 0;
+	const int IdValid = 1;
+	const int IdInvalid = 2;
 
-string IdentifierFSM (const string& input){
-	int state = IdBeginning;
-	
-	for (size_t  i = 0; i < input.size(); i++){
-		char ch = input[i];
-		
-		switch (state) {
+	string IdentifierFSM(const string &input)
+	{
+		int state = IdBeginning;
+
+		for (size_t i = 0; i < input.size(); i++)
+		{
+			char ch = input[i];
+
+			switch (state)
+			{
 			case IdBeginning:
-			if (isalpha(ch) == true) {
-				state = IdValid;
-			} else {
-				state = IdInvalid;
-			}
-			break;
-				
+				if (isalpha(ch) == true)
+				{
+					state = IdValid;
+				}
+				else
+				{
+					state = IdInvalid;
+				}
+				break;
+
 			case IdValid:
-			if (isalnum(ch) == true) {
-				state = IdValid;
-			} else {
-				state = IdInvalid;
-			}
-			break;
-				
+				if (isalnum(ch) == true)
+				{
+					state = IdValid;
+				}
+				else
+				{
+					state = IdInvalid;
+				}
+				break;
+
 			case IdInvalid:
 				break;
+			}
+			if (state == IdInvalid)
+			{
+				break;
+			}
 		}
-		if (state == IdInvalid) {break;}
+
+		if (state == IdValid)
+		{
+			return "identifier " + input;
+		}
+		else
+		{
+			return "invalid";
+		}
 	}
-	
-	if (state == IdValid) {
-		return "identifier " + input;
-	} else { return "invalid"; }
-		
-	
-}
 
-const int NumBeginning = 0;
-const int NumInt = 1;
-const int NumReal = 2;
-const int NumInvalid = 3;
+	const int NumBeginning = 0;
+	const int NumInt = 1;
+	const int NumReal = 2;
+	const int NumInvalid = 3;
 
-string NumberFSM (const string& input){
-	int state = NumBeginning;
-	
-	for (size_t  i = 0; i < input.size(); i++){
-		char ch = input[i];
-		
-		switch (state) {
+	string NumberFSM(const string &input)
+	{
+		int state = NumBeginning;
+
+		for (size_t i = 0; i < input.size(); i++)
+		{
+			char ch = input[i];
+
+			switch (state)
+			{
 			case NumBeginning:
-			if (isnum(ch) == true) {
-				state = NumInt;
-			} else if (ch == ".") {
-				state = NumReal;
-			} else {
-				state = NumInvalid;
-			}
-			break;
-				
+				if (isdigit(ch) == true)
+				{
+					state = NumInt;
+				}
+				else if (ch == '.')
+				{
+					state = NumReal;
+				}
+				else
+				{
+					state = NumInvalid;
+				}
+				break;
+
 			case NumInt:
-			if (isnum(ch) == true) {
-				state = NumInt;
-			} else if (ch == ".") {
-				state = NumReal;
-			} else {
-				state = NumInvalid;
-			}
-			break;
-			
+				if (isnum(ch) == true)
+				{
+					state = NumInt;
+				}
+				else if (ch == '.')
+				{
+					state = NumReal;
+				}
+				else
+				{
+					state = NumInvalid;
+				}
+				break;
+
 			case NumReal:
-			if (isnum(ch) == true) {
-				state = NumReal;
-			} else {
-				state = NumInvalid;
-			}
-			
+				if (isnum(ch) == true)
+				{
+					state = NumReal;
+				}
+				else
+				{
+					state = NumInvalid;
+				}
+
 			case NumInvalid:
 				break;
+			}
+			if (state == NumInvalid)
+			{
+				break;
+			}
 		}
-		if (state == NumInvalid) {break;}
+
+		if (state == NumInt)
+		{
+			return "integer " + input;
+		}
+		else if (state == NumReal)
+		{
+			return "real " + input;
+		}
+		else
+		{
+			return "invalid";
+		}
 	}
-	
-	if (state == NumInt) {
-		return "integer " + input;
-	} else if (state == NumReal) {
-		return "real " + input;
-	} else { return "invalid"; }
-		
-	
-}
 
+	int main()
+	{
+		// main logic will go here.
+		ifstream myFile;
+		Token t;
 
-int main(){
-   //main logic will go here. 
+		while (!myFile.eof())
+		{
+			t = lexer(myFile);
+			cout << "<" << t.token << ", " << t.lexeme << ">" << endl;
+		}
 
-    ifstream myfile;
-    myfile.open("Rat25f.txt");
-    if(!myfile){
-        cout<<"File not found"<<endl;
-        return -1;
-    }
-    while(!myfile.eof()){
-        char token[20];
-        char lexeme[20];
-        lexer(token);
-        cout<<token<<" "<<lexeme<<endl;
-    }
-
-   return 0;
-}
+		return 0;
+	}
